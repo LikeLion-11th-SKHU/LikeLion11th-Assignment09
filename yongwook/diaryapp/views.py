7b4811f5385e3fc17df7084f33b3e9b1a7bf7a9c
kimyongwook98 <rladyddnr0209@naver.com>
2023-06-22 16:57:11 +0900

    1차 과제 제출

---
+++ yongwook/diaryapp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from .models import DiaryNotForm
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def new(request): 
+    return render(request, 'new.html')
+
+def create(request):
+    diary = DiaryNotForm()
+    diary.title = request.POST.get('title', '')
+    diary.pub_date = timezone.now()
+    diary.weather = request.POST.get('weather', '')
+    diary.content = request.POST.get('content', '')
+    diary.file = request.FILES.get('file')
+    diary.save()
+    return redirect('notform')
+
+def notform(request):
+    diarys = DiaryNotForm.objects
+    return render(request, 'notform.html', {'diarys': diarys})
+
+def detail(request, id):
+    diary = get_object_or_404(DiaryNotForm, id = id)
+    return render(request, 'detail.html', {'diary': diary})
+
+def edit(request, id):
+    edit_diary = DiaryNotForm.objects.get(id = id)
+    return render(request, 'edit.html', {'edit_diary': edit_diary})
+
+def update(request, id):
+    update_diary = DiaryNotForm.objects.get(id = id)
+    update_diary.title = request.POST['title']
+    update_diary.pub_date = timezone.now()
+    update_diary.weather = request.POST['weather']
+    update_diary.content = request.POST['content']
+    if 'file' in request.FILES:
+        update_diary.file = request.FILES['file']
+    update_diary.save()
+    return redirect('notform')
+
+def delete(request, id):
+    delete_diary = get_object_or_404(DiaryNotForm, id = id)
+    delete_diary.delete()
+    return redirect('notform')
---
+++ yongwook/journalapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+import journalapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('write/', journalapp.views.write, name='write'),
+    path('useform/', journalapp.views.useform, name='useform'),
+    path('detail2/<int:id>/', journalapp.views.detail2, name='detail2'),
+    path('update2/<int:id>/', journalapp.views.update2, name='update2'),
+    path('delete2/<int:id>/', journalapp.views.delete2, name='delete2'),
+]
