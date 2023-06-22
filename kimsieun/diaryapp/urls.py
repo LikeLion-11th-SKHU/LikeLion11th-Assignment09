ac4edf0752793ce495622c6eafef04df17252eed
kimsieun99 <dotoryeop@gmail.com>
2023-06-22 23:52:03 +0900

    1

---
+++ kimsieun/diaryapp/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+import diaryapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('new/', diaryapp.views.new, name = 'new'),
+    path('new/create/', diaryapp.views.create, name = 'create'),
+    path('diary/', diaryapp.views.diary, name='diary'),
+    path('detail/<int:id>', diaryapp.views.detail, name='detail'),
+    path('edit/<int:id>', diaryapp.views.edit, name='edit'),
+    path('update/<int:id>', diaryapp.views.update, name='update'),
+    path('delete/<int:id>', diaryapp.views.delete, name='delete'),
+    
+]
---
+++ kimsieun/journalapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class JournalUseForm(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    weather = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('data published')
+    image = models.ImageField(upload_to='photo/', blank=True)
+    
+    def __int__(self):
+        return self.content
